1878e4ba6cc0a23d6585fa63a76d12bb402201d5
ik315717 <ian.l.kennedy93@gmail.com>
2015-11-10 20:07:45 -0500

    Small Update 11/9/15
    Added basic Main Menu/Game Over Screen. Started searching for background
    music/sounds to use.
    
    Next steps: Implement timer (which will affect enemy movement). create
    sounds/music. Various other tweaks.

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public Texture background_img;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnGUI()
+    {
+        // Create background, make it fit any screen size
+
+        GUI.DrawTexture(new Rect(0f, 0f, Screen.width, Screen.height), background_img);
+
+        GUI.skin.button.fontSize = 50;
+        if (GUI.Button(new Rect(Screen.width/4.0f, Screen.height / 2.0f, Screen.width / 2.0f, Screen.height / 8.0f), "Play!"))
+            Application.LoadLevel("GameScreen");
+    }
+}
---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class GameOverScript : MonoBehaviour
+{
+
+    int score = 0;
+    private float GameOverX, GameOverY, GameOverWidth, GameOverHeight = 0.0f;
+    private float ButtonX, ButtonY, ButtonWidth, ButtonHeight = 0.0f;
+    private float ScoreX, ScoreY, ScoreWidth, ScoreHeight = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Keep screen in portrait mode for mobile
+        Screen.orientation = ScreenOrientation.Portrait;
+        score = PlayerPrefs.GetInt("score");
+    }
+
+    void OnGUI()
+    {
+        ButtonHeight = Screen.height / 8.0f;
+        ButtonWidth = Screen.width / 2.0f;
+        ButtonX = Screen.width / 2.0f - ButtonWidth / 2.0f;
+        ButtonY = Screen.height / 2.0f;
+
+        GUI.skin.button.fontSize = 50;
+        if (GUI.Button(new Rect(ButtonX, ButtonY - ButtonHeight, ButtonWidth, ButtonHeight), "Retry?"))
+            Application.LoadLevel("GameScreen");
+        if (GUI.Button(new Rect(ButtonX, ButtonY, ButtonWidth, ButtonHeight), "Main Menu"))
+            Application.LoadLevel("MainMenuScene");
+
+        GameOverWidth = Screen.width / 2f;
+        GameOverHeight = Screen.height / 10.0f;
+        GameOverX = Screen.width / 2.0f - GameOverWidth / 2.0f;
+        GameOverY = Screen.height / 8.0f;//ButtonY - 2.0f * GameOverHeight;
+
+        GUI.skin.label.fontSize = 40;
+        GUI.Label(new Rect(GameOverX, GameOverY, GameOverWidth, GameOverHeight), "GAME OVER");
+
+        ScoreHeight = Screen.height / 10.0f;
+        ScoreWidth = Screen.width / 2.0f;
+        ScoreX = GameOverX;
+        ScoreY = GameOverY - GameOverHeight;
+
+        GUI.Label(new Rect(ScoreX, ScoreY, ScoreWidth, ScoreHeight), "Score: " + score);
+    }
+
+}
